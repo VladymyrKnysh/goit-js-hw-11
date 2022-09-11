3a9443917f0515850f39db2fb174d0d5cd284b1e
VladymyrKnysh <99245967+VladymyrKnysh@users.noreply.github.com>
2022-09-11 16:50:48 +0300

    correct11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-// import simpleLightbox from "simplelightbox";
-// import "simplelightbox/dist/simple-lightbox.min.css";
+import simpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import axios from "axios";
 import { Notify } from "notiflix";
 
@@ -15,9 +15,16 @@ let totalHits = 0
 const per_page = 40
 const BASE_URL = 'https://pixabay.com/api/'
 const KEY = '29578130-7e7d5768f03af60ad584cdcef'
+let lightbox = {}
+const optionsLightbox = {    
+    'captionsData': 'alt',
+    'captionPosition': 'bottom',
+    'captionDelay': 250 
+  }
 
 refs.form.addEventListener('submit', onFormSubmit)
  
+
 function onFormSubmit(e) {
   e.preventDefault()
   refs.gallery.innerHTML = ''
@@ -26,24 +33,30 @@ function onFormSubmit(e) {
   if (inputValue === '') {
     Notify.warning('Поле поиска должно быть заполнено');
     return
-  }
+  }  
   fetchImages(inputValue)
 }
+
 
 async function fetchImages(inputValue) {
 try {
   const response = await axios
     .get(`${BASE_URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
   renderList(response.data.hits)
-   totalHits = response.data.totalHits
+   lightbox = new SimpleLightbox('.gallery a', optionsLightbox)
+  totalHits = response.data.totalHits
   page += 1
-  Notify.success(`Hooray! We found ${totalHits} images.`)
+  if (totalHits !== 0) {
+     Notify.success(`Hooray! We found ${totalHits} images.`)
+  }
+ 
 } catch (error) {
   console.log(error);
 }
 }
 
 function renderList(images) {
+  console.log(images);
   if (images.length === 0) {
     refs.gallery.innerHTML = ''
     refs.loadmoreBtn.classList.add('visually-hidden-js')
@@ -52,8 +65,10 @@ function renderList(images) {
    }
   const markup = images
     .map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads}) => {
-      return `<div class="photo-card">
+      return `  <div class="photo-card">
+    <a href="${largeImageURL}">
   <img src="${webformatURL}" alt="${tags}" loading="lazy"/>
+  </a>
    <div class="info">
      <p class="info-item">
        <b>Likes</b> <br>
@@ -76,23 +91,24 @@ function renderList(images) {
     })
         .join("");
   refs.gallery.insertAdjacentHTML("beforeend", markup)
-   refs.loadmoreBtn.classList.remove('visually-hidden-js')
+  refs.loadmoreBtn.classList.remove('visually-hidden-js')
 }
 
 refs.loadmoreBtn.addEventListener('click', onLoadmoreClick)
 
 async function onLoadmoreClick() { 
+  
   const totalPages = Math.ceil(totalHits / per_page)
   if (page > totalPages) {
     refs.loadmoreBtn.classList.add('visually-hidden-js')
-     refs.gallery.innerHTML = ''
     Notify.info("We're sorry, but you've reached the end of search results.")
     return
   }
   try {
   const response = await axios
     .get(`${BASE_URL}?key=${KEY}&q=${inputValue}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`)
-  renderList(response.data.hits)
+    renderList(response.data.hits)
+    lightbox.refresh()
    page += 1
 } catch (error) {
   console.log(error);
